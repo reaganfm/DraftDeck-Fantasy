e841fd0a5fb5ce09329ec86c5843067dcf4fc932
Derek Nguyen <148355684+dereknguyen08@users.noreply.github.com>
2025-03-13 22:56:26 -0400

    final fix

--- DraftDeckFantasy 2-1-25/DraftDeckFantasy 2-1-25/search.js
+++ DraftDeckFantasy 2-1-25/DraftDeckFantasy 2-1-25/search.js
@@ -56,7 +56,15 @@ document.addEventListener("DOMContentLoaded", function () {
       resultsContainer.style.textAlign = "center"; 
     }
   } else {
-    document.querySelector(".group").textContent = "Please enter a search term.";
+    const resultsContainer = document.querySelector(".group");
+    if(resultsContainer){ // check if resultsContainer exists.
+      resultsContainer.textContent = "Please enter a search term.";
+      resultsContainer.style.color = "#333";
+      resultsContainer.style.fontFamily = "Arial, sans-serif";
+      resultsContainer.style.fontSize = "40px";
+      resultsContainer.style.padding = "150px";
+      resultsContainer.style.textAlign = "center";
+    }
   }
 
   // Search box and filter functionality from home.js
